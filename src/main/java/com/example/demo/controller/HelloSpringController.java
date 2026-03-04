2a01b16e845e946a0bfb1a6ba1a8adb3876233c5
1m4nim <fancustard9@gmail.com>
2026-03-04 19:07:03 +0900

    MAVENにライブラリ書くだけじゃだめでcleanしてinstallしなきゃだめ

--- src/main/java/com/example/demo/controller/HelloSpringController.java
+++ src/main/java/com/example/demo/controller/HelloSpringController.java
@@ -1,20 +1,37 @@
+// package com.example.demo.controller;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// //import org.springframework.web.bind.annotation.RequestParam;
+
+// @Controller
+// public class HelloSpringController {
+
+//     @GetMapping("/hello")
+//     // public String getMethodName(@RequestParam String param) {
+//     //     return new String();
+//     // }
+
+//     public String hello(){
+//         //HTMLテンプレートにhello-spring.htmlを指定
+//         return "hello-spring";
+//     }
+// }
+
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class HelloSpringController {
 
+    /**
+     * http://localhost:8080/hello にアクセスした時に実行される
+     */
     @GetMapping("/hello")
-    // public String getMethodName(@RequestParam String param) {
-    //     return new String();
-    // }
-    
-    public String hello(){
-        //HTMLテンプレートにhello-spring.htmlを指定
+    public String hello() {
+        // templates/hello-spring.html を探しに行く
         return "hello-spring";
     }
 }
